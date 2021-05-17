c837351ff99fdc7b774094ff8fba0f88ed592463
Артем Скачков <topkzzz@gmail.com>
2021-05-17 21:17:44 +0300

    Add tests to workflow

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,23 +1,24 @@
 from http import HTTPStatus
 
 from django.test import Client, TestCase
+from django.urls import reverse
 
 
 class StaticPagesURLTests(TestCase):
+    @classmethod
     def setUp(self):
         self.guest_client = Client()
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('/')
+        response = self.guest_client.get(reverse('infra_app:index'))
         self.assertEqual(response.status_code, HTTPStatus.OK)
-
-        response = self.guest_client.get('/second_page/')
+        response = self.guest_client.get(reverse('infra_app:second_page'))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_context(self):
         """Проверка контекста страниц."""
-        response = self.guest_client.get('/')
+        response = self.guest_client.get(reverse('infra_app:index'))
         self.assertContains(response, 'У меня получилось!')
-        response = self.guest_client.get('/second_page/')
+        response = self.guest_client.get(reverse('infra_app:second_page'))
         self.assertContains(response, 'А это вторая страница!')
