f50cb258c8751082155f7720ee37e90c5027dcdc
Артем Скачков <topkzzz@gmail.com>
2021-05-17 19:03:28 +0300

    First commit

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!😿🤴')
 
 
 def second_page(request):
